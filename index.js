659666d49c3999b4a70bf663b101e562f5c420bc
seenso <>
2021-11-01 09:16:52 -0800

    passed test for random

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // Write your code here
+
+//multiply
 const num1 = Number(31);
 const num2 = Number(2);
 let multiply = Number(num1) * Number(num2);
+
+//random
+let random = Math.floor(Math.random() * 100) + 1; //gen num between 1 and 100
+
+//mod
+
+
+//max
